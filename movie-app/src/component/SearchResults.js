df067630178a0b6af6e750f379e7f3386959c912
ackottsi <kottsickandrewc@johndeere.com>
2021-02-24 22:00:15 -0600

    added explanation for ternary in SearchResults render

--- movie-app/src/component/SearchResults.js
+++ movie-app/src/component/SearchResults.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Search from './Search';
-import { Route, Switch, Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../SearchResults.css'
 
@@ -30,106 +29,95 @@ class SearchResults extends Component {
                                   //of the string.  The boolean value of searchResponse is used below
                                   //in the render for a conditional rendering condition  
   
-     this.setState ({
-      searchTerm: movieSearch,
-      movieData: movieData1.data.Search,
-      apiDataLoaded: true,
-      searchResponse:false
-    })
-  }
-    else {this.setState ({
-      searchTerm: movieSearch,
-      movieData: movieData1.data.Search,
-      apiDataLoaded: true,
-      searchResponse:true
-    })
+        this.setState ({
+          searchTerm: movieSearch,
+          movieData: movieData1.data.Search,
+          apiDataLoaded: true,
+          searchResponse:false
+        });
     }
 
+    else {this.setState ({
+        searchTerm: movieSearch,
+        movieData: movieData1.data.Search,
+        apiDataLoaded: true,
+        searchResponse:true
+            })};
+    }
+
+
+  
+      //component did Update handles all subsequent searches after initial search
+      componentDidUpdate= async (prevProps)=>{
+
+        if(this.props.location.state.title!==prevProps.location.state.title){
+      
+          const movieSearch = this.props.location.state.title
+          const movieData1 = await axios.get(`http://www.omdbapi.com/?apikey=38e29c7e&s=${movieSearch}`);
+          const resultsString=movieData1.data.Response;
+
+          if(resultsString==="False"){
+        
+            this.setState ({
+            searchTerm: movieSearch,
+            movieData: movieData1.data.Search,
+            apiDataLoaded: true,
+            searchResponse:false
+            })
+          }
+
+          else {this.setState ({
+            searchTerm: movieSearch,
+            movieData: movieData1.data.Search,
+            apiDataLoaded: true,
+            searchResponse:true
+                  })};  
+      }
   }
-
-
   
-  //component did Update handles all subsequent searches after initial search
-  componentDidUpdate= async (prevProps)=>{
-    
-    console.log(prevProps.location)
-    console.log(this.props.location)
-    if(this.props.location.state.title!==prevProps.location.state.title){
+//added the ternary below in render to prevent "cannot map undefined error".  when the api is queried, if there are too many results, or no results, it returns a response value.  the logic statements in componentDidMount and componentDidUpdate put this value to use.  if the query to the data base returns false, a message of "No Results Found" will be displayed.
+
       
-    // console.log("component did update ran!")
-    const movieSearch = this.props.location.state.title
-    const movieData1 = await axios.get(`http://www.omdbapi.com/?apikey=38e29c7e&s=${movieSearch}`)
-    const resultsString=movieData1.data.Response;
-    console.log(movieData1.data.Response)
-
-    if(resultsString==="False"){
-  
-      this.setState ({
-       searchTerm: movieSearch,
-       movieData: movieData1.data.Search,
-       apiDataLoaded: true,
-       searchResponse:false
-     })
-   }
-     else {this.setState ({
-       searchTerm: movieSearch,
-       movieData: movieData1.data.Search,
-       apiDataLoaded: true,
-       searchResponse:true
-           })
-        }
- 
-     }
- 
-   }
-  
-      
-  render() {
-       
+  render() {  
     return (
       <div>
-            <div>
+          <div>
               {this.state.searchResponse ? 
-              <div className="resultsList">
-                <h1>{`search results for "${this.state.searchTerm}"...`}</h1>
-                {this.state.movieData.map(movie => (
+            <div className="resultsList">
+                    <h1>{`search results for "${this.state.searchTerm}"...`}</h1>
+                    {this.state.movieData.map(movie => (
                   <div key={movie.imdbID} className="resultsCard">
-                <div className="resultsCardImageContainer">
-                  <Link 
-                    to={{
-                      pathname: `/SearchDetail/`,
-                      state: { titledata: movie.imdbID },
-                    }}>
-                      <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-                  </Link>
-                  {/* {console.log(movie.imdbID)} */}
-                </div>
-                <Link 
-                    to={{
-                      pathname: `/SearchDetail/`,
-                      state: { titledata: movie.imdbID },
-                    }}>
-                    <div className="resultsCardTitle">
-                      {movie.Title}
-                    </div>
-                </Link>
-              </div>
-                ))}
-              </div>
-              :
-              <h3>data not loaded</h3>
-              }
+                      <div className="resultsCardImageContainer">
+                        <Link 
+                          to={{
+                            pathname: `/SearchDetail/`,
+                            state: { titledata: movie.imdbID },
+                          }}>
+                            <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+                        </Link>
+                        {/* {console.log(movie.imdbID)} */}
+                      </div>
+                    <Link 
+                        to={{
+                          pathname: `/SearchDetail/`,
+                          state: { titledata: movie.imdbID },
+                        }}>
+                        <div className="resultsCardTitle">
+                          {movie.Title}
+                        </div>
+                    </Link>
+                  </div>
+                    ))}
             </div>
+              :
+              <h3>No Results Found</h3>
+              }
+          </div>
       </div>         
-  
-
-          )
-
-
-
+    )
   }
-
 }
+
 export default SearchResults;
 
 
